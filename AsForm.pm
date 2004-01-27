dbbca347693970baa591bbcdc6adb545edf999a1
Simon Cozens <simon@cpan.org>
2004-01-27 19:15:10 +0000

    Import of SIMON/Class-DBI-AsForm-2.0 from CPAN.
    gitpan-cpan-distribution: Class-DBI-AsForm
    gitpan-cpan-version:      2.0
    gitpan-cpan-path:         SIMON/Class-DBI-AsForm-2.0.tar.gz
    gitpan-cpan-author:       SIMON
    gitpan-cpan-maturity:     released

--- AsForm.pm
+++ AsForm.pm
@@ -2,13 +2,14 @@ package Class::DBI::AsForm;
 use 5.006;
 use strict;
 use warnings;
+our $OLD_STYLE = 0;
 
+use HTML::Element;
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw( to_cgi to_field _to_textarea _to_textfield _to_select
 type_of );
-our $VERSION = '1.2';
-use CGI qw/:standard/;
+our $VERSION = '2.0';
 my %types_cache;
 
 =head1 NAME
@@ -20,14 +21,13 @@ Class::DBI::AsForm - Produce HTML form elements for database columns
     package Music::CD;
     use Class::DBI::AsForm;
     use base 'Class::DBI';
-    use CGI qw/:standard/;
     ...
 
     sub create_or_edit {
         my $class = shift;
         my %cgi_field = $class->to_cgi;
         return start_form,
-               (map { "<b>$_</b>: ". $cgi_field{$_}." <br>" } $class->Columns),
+               (map { "<b>$_</b>: ". $cgi_field{$_}->as_HTML." <br>" } $class->Columns),
                end_form;
     }
 
@@ -56,8 +56,8 @@ C<Class::DBI>-derived class.
 
 =head2 to_cgi
 
-This returns a hash mapping all the column names of the class to HTML 
-snippets.
+This returns a hash mapping all the column names of the class to
+HTML::Element objects representing form widgets.
 
 =cut
 
@@ -115,25 +115,33 @@ sub _fill_cache {
 
 sub _to_textarea {
     my ($self, $col) = @_;
-    return textarea(-name => $col, -default => (ref $self && $self->$col));
+    my $a = HTML::Element->new("textarea", name => $col);
+    if (ref $self) { $a->push_content($self->$col) }
+    $OLD_STYLE && return $a->as_HTML;
+    $a;
 }
 
 sub _to_textfield {
     my ($self, $col) = @_;
-    return textfield(-name => $col, -default => (ref $self && $self->$col));
+    my $a = HTML::Element->new("input", type=> "text", name => $col,
+                       value => (ref $self && $self->$col));
+    $OLD_STYLE && return $a->as_HTML;
+    $a;
 }
 
 sub _to_select {
     my ($self, $col, $hint) = @_;
     my $has_a_class = $hint || $self->__hasa_rels->{$col}->[0];
     my @objs = $has_a_class->retrieve_all;
-    my $sel = -1;
-    if (ref $self and $self->$col()) { $sel = $self->$col()->id }
-    my %labels = map { $_->id => "".$_ } @objs;
-    return popup_menu( -name => $col,
-                       -values => [ map {$_->id} @objs ],
-                       -default => $sel,
-                       -labels => \%labels );
+    my $a = HTML::Element->new("select", name => $col);
+    for (@objs) { 
+        my $sel = HTML::Element->new("option", value => $_->id);
+        $sel->attr("selected" => "selected") if ref $self and $_->id == $self->id;
+        $sel->push_content($_->stringify_self);
+        $a->push_content($sel);
+    }
+    $OLD_STYLE && return $a->as_HTML;
+    $a;
 }
 
 
@@ -141,12 +149,19 @@ sub _to_select {
 
 1;
 
+=head1 CHANGES
+
+Version 1.x of this module returned raw HTML instead of C<HTML::Element>
+objects, which made it harder to manipulate the HTML before sending it
+out. If you depend on the old behaviour, set C<$Class::DBI::AsForm::OLD_STYLE>
+to a true value.
+
 =head1 AUTHOR
 
-Simon Cozens, C<simon@kasei.com>
+Simon Cozens, C<simon@cpan.org>
 
 =head1 SEE ALSO
 
-L<Class::DBI>, L<Class::DBI::FromCGI>.
+L<Class::DBI>, L<Class::DBI::FromCGI>, L<HTML::Element>.
 
 =cut
--- test.pl
+++ test.pl
@@ -10,23 +10,23 @@ package Foo;
 use Test::More tests => 4;
 use base 'Class::DBI';
 use_ok("Class::DBI::AsForm");
+$Class::DBI::AsForm::OLD_STYLE=1;
 *type_of = sub { "varchar" };
 
 Foo->columns(All => qw/id bar baz/);
-like(Foo->to_field("baz"), qr/<input type="text" name="baz"/,
+like(Foo->to_field("baz"), qr/<input .*name="baz"/,
     "Ordinary text field OK");
 
 Foo->has_a(bar => Bar);
-is(Foo->to_field("bar"), '<select name="bar">
-<option value="1">Hi</option>
-</select>',
+is(Foo->to_field("bar"), "<select name=\"bar\"><option value=1>Hi</option></select>\n",
     "Select OK");
 
 my $x = bless({id => 1, bar => Bar->retrieve_all(), baz => "Hello there"}, "Foo");
-my %cgi = ( id => '<input type="text" name="id" value="1" />',
-    bar => '<select name="bar">
-<option selected="selected" value="1">Hi</option>
-</select>',
-            baz => '<input type="text" name="baz" value="Hello there" />'
+my %cgi = ( id => '<input name="id" type="text" value=1>
+',
+    bar => '<select name="bar"><option selected value=1>Hi</option></select>
+',
+            baz => '<input name="baz" type="text" value="Hello there">
+'
           );
 is_deeply({$x->to_cgi}, \%cgi, "All correct as an object method");
